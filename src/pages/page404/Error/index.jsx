f2d4ca7e6c80740319872a5c8484c2b44c2d08d7
PIRAS <metaliansurf@hotmail.fr>
2023-09-01 15:20:14 +0200

    il manquait un espace entre 2 mots sur la page 404

--- src/pages/page404/Error/index.jsx
+++ src/pages/page404/Error/index.jsx
@@ -6,7 +6,7 @@ function Error() {
     <div className="error">
       <div className="error404">404</div>
       <div className="oups">
-        <div className="oups1"> Oups. La page que </div>
+        <div className="oups1"> Oups. La page que &nbsp; </div>
         <div className="oups2"> vous demandez n'existe pas.</div>
       </div>
       <div>
